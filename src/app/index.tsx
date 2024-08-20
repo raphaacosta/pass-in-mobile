76a6b010612eff252456e037916db522e7ff9886
Raphael Costa <rafa.costa0@hotmail.com>
2024-08-20 16:32:54 -0300

    feat: create home page(partial)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { Input } from "@/components/Input";
+import { colors } from "@/styles/colors";
 
 export default function Home() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center">
-      <Text className="text-white font-bold">Hello React Native</Text>
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field
+            placeholder="Código do ingresso"
+          />
+        </Input>
+      </View>
     </View>
   )
 }
