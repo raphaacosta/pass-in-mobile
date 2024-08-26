c282aa8202b45ffdd6f37382acd6b8ef50bad014
Raphael Costa <rafa.costa0@hotmail.com>
2024-08-26 17:14:12 -0300

    feat: create header component

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -6,7 +6,7 @@ export default function Ticket() {
   return (
     <View className="flex-1 bg-green-500">
       <StatusBar barStyle="light-content" />
-      {/* <Header title="Minha credencial" /> */}
+      <Header title="Minha credencial" />
     </View>
   )
 }
