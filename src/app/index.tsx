3c9739cdc3a78a8149f4ab8463eed8fd8a6008e7
Raphael Costa <rafa.costa0@hotmail.com>
2024-08-26 16:54:03 -0300

    feat: add basic validation to credentials in first page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
-import { View, Image, StatusBar } from "react-native";
+import { View, Image, StatusBar, Alert } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 
 import { Input } from "@/components/Input";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/Button";
+import { useState } from "react";
 
 export default function Home() {
+  const [code, setCode] = useState("");
+
+  function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert("Credencial", "Informe o código do ingresso!");
+    }
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -25,9 +34,11 @@ export default function Home() {
           />
           <Input.Field
             placeholder="Código do ingresso"
+            onChangeText={setCode}
           />
         </Input>
         <Button
+          onPress={handleAccessCredential}
           title="Acessar credencial"
         />
         <Link
