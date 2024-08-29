8d8ccc05777524fb5a18e46a02bcd251e24d25b7
Raphael Costa <rafa.costa0@hotmail.com>
2024-08-29 13:04:51 -0300

    feat: finish ticket's screen front by adding scrollview

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,14 +1,53 @@
-import { StatusBar, View } from "react-native";
+import { StatusBar, Text, View, ScrollView, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-import { Header } from "../components/Header";
+import { Header } from "@/components/Header";
 import { Credential } from "@/components/credential";
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/Button";
 
 export default function Ticket() {
   return (
     <View className="flex-1 bg-green-500">
       <StatusBar barStyle="light-content" />
+
       <Header title="Minha credencial" />
-      <Credential />
+      <ScrollView
+        className="-mt-28 -z-10"
+        contentContainerClassName="px-8 pb-8"
+        showsVerticalScrollIndicator={false}
+      >
+        <Credential />
+
+        <FontAwesome
+          name="angle-double-down"
+          size={24}
+          color={colors.gray[300]}
+          className="self-center my-6"
+        />
+
+        <Text className="text-white font-bold text-2xl mt-4">
+          Compartilhar credencial
+        </Text>
+
+        <Text className="text-white font-regular text-base mt-1 mb-6">
+          Mostre ao mundo que você vai participar do Unite Summit!
+        </Text>
+
+        <Button title="Compartilhar" />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={{
+            marginTop: 40,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Text className="text-base text-white font-bold text-content">Remover Ingresso</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   )
 }
