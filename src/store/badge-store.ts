08d146827117f2e023a3943dfd8871f14d3c72ee
Raphael Costa <rafa.costa0@hotmail.com>
2024-09-27 15:13:53 -0300

    feat: create store for global state menagement and data persistance

---
+++ src/store/badge-store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type BadgeStore = {
+  id: string;
+  name: string;
+  email: string;
+  eventTitle: string;
+  checkInURL: string;
+  image?: string;
+}
+
+type StateProps = {
+  data: BadgeStore | null;
+  save: (data: BadgeStore) => void;
+}
+
+export const useBadgeStore = create(
+  persist<StateProps>((set) => ({
+    data: null,
+
+    save: (data: BadgeStore) => set(() => ({ data })),
+  }), {
+    name: "nlw-unite:badge",
+    storage: createJSONStorage(() => AsyncStorage),
+  }));
