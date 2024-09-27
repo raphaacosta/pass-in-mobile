8a033002e1837239c09fed8055ca6d9c1d32ebf4
Raphael Costa <rafa.costa0@hotmail.com>
2024-09-27 16:36:47 -0300

    feat: add ticket animation

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,15 +1,18 @@
 import {
   Image,
-  ImageBackground,
   Text,
+  View,
+  ImageBackground,
   TouchableOpacity,
-  View
+  useWindowDimensions,
 } from "react-native";
+import { MotiView } from "moti";
 
 import { Feather } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { QRCode } from "@/components/Qrcode";
 import { BadgeStore } from "@/store/badge-store";
+import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 type Props = {
   data: BadgeStore;
@@ -19,8 +22,34 @@ type Props = {
 }
 
 export function Credential({ onChangeAvatar, onExpandeQRCode, data }: Props) {
+  const { height, width } = useWindowDimensions();
+
   return (
-    <View className="w-full self-stretch items-center ">
+    <MotiView
+      className="w-full self-stretch items-center"
+      from={{
+        opacity: 0,
+        translateY: -height,
+        rotateZ: "50deg",
+        rotateY: "30deg",
+        rotateX: "30deg",
+      }}
+      animate={{
+        opacity: 1,
+        translateY: 0,
+        rotateZ: "0deg",
+        rotateY: "0deg",
+        rotateX: "0deg",
+      }}
+      transition={{
+        type: "spring",
+        damping: 20,
+        rotateZ: {
+          damping: 15,
+          mass: 3
+        },
+      }}
+    >
       <Image
         source={require("@/assets/ticket/band.png")}
         className="w-24 h-52 z-10"
@@ -94,6 +123,6 @@ export function Credential({ onChangeAvatar, onExpandeQRCode, data }: Props) {
           </Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   )
 }
